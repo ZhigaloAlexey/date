eb973eec7febdc12d842242576bf3a519bfab471
ZhigaloAlexey <zhigalo_alexey@bigmir.net>
2013-05-17 09:37:58 +0300

    Update 17.05

---
+++ date.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std; 
+
+class Date
+{
+private:
+  int  AMonth;
+	int day;
+	int year;
+public:
+	void setDate(int Mnth, int dy, int yr)
+	{
+		AMonth=Mnth;
+		day=dy;
+		year=yr;
+	}
+	int getDate()
+	{ 
+
+		return AMonth;
+	}
+
+
+		void DisplayDate()
+		{
+			cout<<"DisplayDate: " <<getDate();
+
+		}
+	};
+	int 	main()
+	{
+
+		Date myDate;
+		int Mh;
+		int d;
+		int y;
+		cout<<"Vvedite Mesas \n";
+		cin>> Mh;
+		if (Mh <1 && Mh>12 )
+		{
+			cout<<" Vedite pravilno Month ";
+			Mh=1;
+		}
+		cout<<"Vvedite den \n";
+		cin>> d ;
+		cout<<"Vvedite god \n";
+		cin>> y;
+
+		myDate.setDate( Mh,  d,  y);
+		myDate.DisplayDate( );
+		_getch();
+		return 0;
+
+	}

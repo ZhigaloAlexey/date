bcffb35f02f0541722aa6248538320b1015ad463
ZhigaloAlexey <zhigalo_alexey@bigmir.net>
2013-05-20 09:53:15 +0300

    Create 20.05.13.cpp

---
+++ 20.05.13.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#include "date.h"
+using namespace std;
+
+
+void Date::setDate(int Mh, int dy, int yr)
+  {
+		//
+		if (Mh <1 && Mh >12 )
+		{
+			cout<<" Vedite pravilno Month ";
+			Mh==1;
+		}
+
+		//
+
+		AMonth=Mh;
+		day=dy;
+		year=yr;
+	}
+	int 	main()
+	{
+
+		Date myDate;
+		int Mh;
+		int d;
+		int y;
+		cout<<"Vvedite Mesas \n";
+		cin>> Mh;
+		
+		
+		cout<<"Vvedite den \n";
+		cin>> d ;
+		cout<<"Vvedite god \n";
+		cin>> y;
+
+		myDate.setDate( Mh,  d,  y);
+		myDate.DisplayDate( );
+		_getch();
+		return 0;
+
+	}

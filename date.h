5b8b62cf5946d95365c524494a63e936dad70c55
ZhigaloAlexey <zhigalo_alexey@bigmir.net>
2013-05-20 09:51:57 +0300

    Create date.h

---
+++ date.h
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+using namespace std;
+
+class Date
+{
+private:
+  int  AMonth;
+	int day;
+	int year;
+public:
+	void setDate(int , int , int );
+
+	int getDate();
+
+
+		void DisplayDate();
+		
+	};

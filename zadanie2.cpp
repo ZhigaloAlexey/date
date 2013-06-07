6b6abed290f87a2a294d20814ae6ef309a6f2deb
Zhigalo Alexey OPSIK 2/11k <zhigalo_alexey@bigmir.net>
2013-06-07 11:19:09 +0300

    Create zadanie2.cpp

---
+++ zadanie2.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+
+cout << "********"<<"\t"<<"   ***  "<<  "\t"<<"  *  "<<endl;
+cout << "*      *"<<"\t"<<"  *   * "<< "\t"<< " *** "<<endl;
+cout << "*      *"<<"\t"<<" *     *"<<"\t"<<  "*****"<<endl;  
+cout << "*      *"<<"\t"<<" *     *"<<"\t"<<  "  *  "<<endl;	
+cout << "*      *"<<"\t"<<" *     *"<<"\t"<<  "  *  "<<endl;
+cout << "*      *"<<"\t"<<" *     *"<<"\t"<<  "  *  "<<endl;	
+cout << "*      *"<<"\t"<<"  *   * "<< "\t"<< "  *  "<<endl;
+cout << "********"<<"\t"<<"   ***  "<<  "\t"<<"  *  "<<endl;
+
+
+   
+ 
+
+
+getch();
+	return 0;
+}
